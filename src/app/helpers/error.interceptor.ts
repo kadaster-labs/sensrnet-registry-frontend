14459e12c91a06ef9548d94744570e99bca1937a
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2020-06-16 17:18:14 +0200

    return user to login page when 401

--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -1,21 +1,24 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-import { environment } from 'src/environments/environment';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+  ) { }
 
   public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError((err) => {
-      if (err.status === 401 && !err.url.startsWith(environment.apiUrl)) {
+      if (err.status === 401 && !this.router.routerState.snapshot.url.startsWith('/login')) {
         // auto logout if 401 response returned from api
         this.authenticationService.logout();
-        location.reload(true);
+        location.reload();
       }
 
       const error = err.error.message || err.statusText;
