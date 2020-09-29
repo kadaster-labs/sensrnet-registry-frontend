854fc015efdda9e0beca33676cf660508fa67db7
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2020-09-29 18:06:46 +0200

    fix headless e2e testing using docker

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  xit('should display welcome message', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('viewer app is running!');
+    expect(page.getTitleText()).toEqual('Welcome to SensRNet');
   });
 
   afterEach(async () => {
