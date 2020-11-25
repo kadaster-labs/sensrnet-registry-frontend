76a8b0f1052417a9843fdd919dca8c9992725e93
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2020-11-25 17:30:04 +0100

    add language button in menu bar

--- src/app/components/language-switcher/language-switcher.component.ts
+++ src/app/components/language-switcher/language-switcher.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-language-switcher',
   templateUrl: './language-switcher.component.html',
+  styleUrls: ['./language-switcher.component.scss'],
 })
 export class LanguageSwitcherComponent {
   allLanguages = [{
