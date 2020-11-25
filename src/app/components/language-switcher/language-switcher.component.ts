25b4796e84daf44e0519134326ab78667406c4d6
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2020-11-25 17:49:16 +0100

    fixed box-shadow removal

--- src/app/components/language-switcher/language-switcher.component.ts
+++ src/app/components/language-switcher/language-switcher.component.ts
@@ -4,7 +4,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-language-switcher',
   templateUrl: './language-switcher.component.html',
-  styleUrls: ['./language-switcher.component.scss'],
 })
 export class LanguageSwitcherComponent {
   allLanguages = [{
