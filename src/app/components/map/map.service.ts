0c2fe2b4347f4f296122d4e66922fa68935eb55e
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2021-04-09 19:33:36 +0200

    fix projection

--- src/app/components/map/map.service.ts
+++ src/app/components/map/map.service.ts
@@ -30,8 +30,9 @@ export class MapService {
    */
   private createMap(id): Map {
     proj4.defs('EPSG:28992',
-      '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 ' +
-      '+y_0=463000 +ellps=bessel +units=m +no_defs');
+    '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.999908 +x_0=155000 ' +
+    '+y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,' +
+    '-1.8703473836068,4.0812 +no_defs');
     register(proj4);
     const dutchProjection = getProjection('EPSG:28992');
     const extent: Extent = [-285401.92, 22598.08, 595401.92, 903401.92];
