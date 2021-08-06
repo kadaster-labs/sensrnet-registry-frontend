ea23f83cb492a2d81af6f7619726661558d5a673
Stefan Bussemaker <Stefan.Bussemaker@kadaster.nl>
2021-08-06 11:44:34 +0200

    set a more prescriptive eslint for better consistency

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+      project: './tsconfig/*.json',
+      sourceType: 'node',
+      ecmaVersion: 2020,
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+      'plugin:prettier/recommended',
+      'prettier',
+  ],
+  root: true,
+  env: {
+      node: true,
+      jest: true,
+  },
+  rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'import/order': [
+          'error',
+          {
+              alphabetize: {
+                  order: 'asc',
+                  caseInsensitive: true,
+              },
+          },
+      ],
+  },
+};
